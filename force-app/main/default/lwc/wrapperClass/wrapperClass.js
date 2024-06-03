acaadfc1a964737e73b3c3c05e97f100dad1edcc
vikash mundari <87792898+vikash24may00@users.noreply.github.com>
2024-06-03 17:11:03 +0530

    first commit

---
+++ force-app/main/default/lwc/wrapperClass/wrapperClass.js
@@ -0,0 +1,25 @@
+import { LightningElement, track, wire } from 'lwc';
+import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities';
+
+export default class OpportunityDataTable extends LightningElement {
+    @track opportunityData = [];
+    @track error;
+
+    columns = [
+        { label: 'Opportunity Name', fieldName: 'opportunityName', type: 'text' },
+        { label: 'Account Name', fieldName: 'accountName', type: 'text' },
+        { label: 'Contact Name', fieldName: 'contactName', type: 'text' }
+    ];
+
+    // Fetch opportunities using wire service
+    @wire(getOpportunities)
+    wiredOpportunities({ error, data }) {
+        if (data) {
+            this.opportunityData = data;
+            this.error = undefined;
+        } else if (error) {
+            this.opportunityData = undefined;
+            this.error = error.body ? error.body.message : error;
+        }
+    }
+}
